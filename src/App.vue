ddee6116d711ada079c22eefce62385bf678f1c9
skchel174 <skchel174@gmail.com>
2021-05-24 12:56:51 +0500

    Add v-main-wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <h1>Hello, Vue</h1>
+    <v-main-wrapper></v-main-wrapper>
   </div>
 </template>
 
 <script>
 
+import vMainWrapper from './components/v-main-wrapper';
+
 export default {
   name: 'App',
   components: {
-
+    vMainWrapper,
   }
 }
 </script>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-main-wrapper">
+    <h1>Hello, Vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-main-wrapper"
+}
+</script>
+
+<style>
+
+</style>
