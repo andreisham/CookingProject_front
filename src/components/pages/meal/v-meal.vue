28a629270ba86ab57790ce7b30860f44ca151c0b
skchel174 <skchel174@gmail.com>
2021-05-24 15:07:47 +0500

    Add pages home & meal

--- src/components/pages/meal/v-meal.vue
+++ src/components/pages/meal/v-meal.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="v-meal">
-    <h1>Meal</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, magnam.</p>
+    <main class="ingredients">
+      <h1 class="heading">Блины с лососем</h1>
+      <h2>Ингредиенты</h2>
+      <ul class="recipe">
+        <li class="recipe__list">мука 1,5 стакана</li>
+        <hr>
+        <li class="recipe__list">мука 1,5 стакана</li>
+        <hr>
+        <li class="recipe__list">мука 1,5 стакана</li>
+        <hr>
+        <li class="recipe__list">мука 1,5 стакана</li>
+        <hr>
+      </ul>
+    </main>
   </div>
 </template>
 
@@ -12,5 +24,23 @@ export default {
 </script>
 
 <style lang="scss">
+.ingredients {
 
+  margin: 14px 40px;
+  color: #434343;
+  .recipe {
+    list-style-image: url("../../../assets/img/list-style.png");
+    hr {
+      margin-left: -22px;
+      width: 361px;
+      height: 1px;
+      background-color: #b9b9b9;
+    }
+    .recipe__list {
+      color: #434343;
+      font-size: 20px;
+      font-weight: 400;
+    }
+  }
+}
 </style>
